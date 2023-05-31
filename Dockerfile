13ba30caf3ae0c364d5adfb4625fb4d1f57acbfb
fr3qu3ncy <kilicbatuhan1205@gmail.com>
2023-05-31 19:50:13 +0200

    Update

--- Dockerfile
+++
@@ -1,9 +0,0 @@
-FROM maven:3.8.3-openjdk-17
-
-ENV JAVA_HOME=/usr/java/openjdk-17
-
-COPY increment_script.sh /increment_script.sh
-
-RUN chmod +x /increment_script.sh
-
-ENTRYPOINT ["/increment_script.sh"]
---
+++ index.js
@@ -0,0 +1,29 @@
+const core = require('@actions/core');
+const execSync = require('child_process').execSync;
+
+try {
+    const actualVersion = core.getInput('actual_version');
+    const latestRelease = core.getInput('latest_release');
+
+    console.log(`Actual version: ${actualVersion}`);
+    console.log(`Latest release: ${latestRelease}`);
+
+    // Get number after last "-" in latestRelease and parse it to int
+    const oldNumber = parseInt(latestRelease.substring(latestRelease.lastIndexOf("-") + 1));
+
+    console.log(`Latest version: ${latestVersion}`);
+
+    // Increment oldNumber
+    const newNumber = oldNumber + 1;
+
+    const newVersion = actualVersion + "-beta-" + newNumber;
+
+    // Set new version in pom.xml
+    execSync('mvn versions:set -DnewVersion=${newVersion} -DgenerateBackupPoms=false');
+
+    core.setOutput("new_version", newVersion);
+
+    console.log(`Set new version to: ${newVersion}`);
+} catch (error) {
+    core.setFailed(error.message);
+}
